7c6fe885c3f1903fd92545b410979c38b6c2ebb7
tareq <tareq236@gmail.com>
2024-03-02 23:20:18 +0600

    1st commit

--- controllers/api/home_page.js
+++ controllers/api/home_page.js
@@ -12,9 +12,9 @@ exports.home_page = async (req, res, next) => {
     })
   }
   const _data_2 = await sequelize.query(`SELECT * FROM donation_career WHERE status = 1 AND is_show_in_home_page = 1 ORDER BY order_by ASC;`, { type: QueryTypes.SELECT });
-  const _data_3 = await sequelize.query(`SELECT el.* FROM event_list el WHERE status = 1 ORDER BY el.id DESC LIMIT 3;`, { type: QueryTypes.SELECT });
+  const _data_3 = await sequelize.query(`SELECT el.* FROM event_list el WHERE status = 1 and event_session != "Upcoming" ORDER BY el.id DESC LIMIT 3;`, { type: QueryTypes.SELECT });
   const _data_4 = await sequelize.query(`SELECT * FROM event_sponsors WHERE status = 1 ORDER BY order_by ASC;`, { type: QueryTypes.SELECT });
-  const _data_5 = await sequelize.query(`SELECT el.* FROM event_list el WHERE status = 1 and event_session != "Upcoming" ORDER BY el.id DESC LIMIT 1;`, { type: QueryTypes.SELECT });
+  const _data_5 = await sequelize.query(`SELECT el.* FROM event_list el WHERE status = 1 ORDER BY el.id DESC LIMIT 1;`, { type: QueryTypes.SELECT });
   const _data_6 = await sequelize.query(`SELECT * FROM publication WHERE status = 1 ORDER BY id DESC LIMIT 3;`, { type: QueryTypes.SELECT });
 
   return res.status(200).json({
